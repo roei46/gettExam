ded6c50be93f54bd23fc5d2dac9b2ac80ff066cc
Roei Baruch <roei.baruch.pepper@gmail.com>
2021-06-07 20:31:25 +0300

    Removing debug and moved scan out

--- GettMobileDelivery/ViewModel/MainViewModel.swift
+++ GettMobileDelivery/ViewModel/MainViewModel.swift
@@ -89,37 +89,40 @@ final class MainVIewModel: MainViewModelType {
         return state.all.count - 1 == state.current
     }
             
+    func scan( state: inout State, action: Action) -> Void {
+        self.myStateObservable.accept(self.isLastItem(state: state))
+        
+        switch action {
+        case .reset(let items):
+            state.all = items
+            state.current = 0
+        case .next:
+            state.current = (state.current + 1) % state.all.count
+            // MARK - Reset when arriving to last point
+        }
+    }
+    
     func bindRx(defaultLocation: CLLocation) -> (_ trigger: Observable<Void>) -> Output {
         { trigger in
             
             // MARK - trigger represents the button tap action
             // MARK - items creates an observable that will make a networking request every time it's subscribed to.
            // items subscrive one time thus for it will make one network call
-            let items = self.networking.loadJSON(type: [NavigationPayload].self).debug("🚘 items")
+            let items = self.networking.loadJSON(type: [NavigationPayload].self)
             // MARK - .merge creates an Observable<Action> that will emit all the values that either of its source observables emit.
             let currentItem = Observable.merge(
                 //MARK - items.map { Action.reset($0) } creates an Observable<Action> that, when subscribed to, will make the network request, wait for the response, then wrap the response in an Action
-                trigger.debug("🚘 merge trigger").map { Action.next },
-                items.debug("🚘 merge items").map { Action.reset($0) }
-            ).debug("🚘 merge all")
-            .scan(into: State()) { state, action in
-                self.myStateObservable.accept(self.isLastItem(state: state))
-                switch action {
-                case .reset(let items):
-                    state.all = items
-                    state.current = 0
-                case .next:
-                    state.current = (state.current + 1) % state.all.count
-                    // MARK - Reset when arriving to last point
-                }
-            }.debug("🚘 scan")
-            .compactMap { $0.all.isEmpty ? nil : $0.all[$0.current] }.debug("🚘 compact")
+                trigger.map { Action.next },
+                items.map { Action.reset($0) }
+            )
+            .scan(into: State(),accumulator: self.scan(state:action:))
+            .compactMap { $0.all.isEmpty ? nil : $0.all[$0.current] }
             .share(replay: 1)
             //MARK - The replay: 1 will store the last emitted value and replay it to any new subscribers
-            let routes = currentItem.debug("🚘 currentItem flat")
+            let routes = currentItem
                 .flatMap { item in
                     self.networking.getRoute(endPoint: Api.getRoute(current: defaultLocation, target: CLLocation(latitude: item.geo.latitue, longitude: item.geo.longitude)), type: Routs.self)
-                }.debug("🚘 routes flat")
+                }
             
             // MARK - The first tap will cause trigger to emit a Void, which will cause map { Action.next } to emit an Action.next which will cause merge to emit an Action.next which will cause scan to emit a State object
             
